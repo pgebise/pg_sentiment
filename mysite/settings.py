2e31bc95f80fa940750d1656666219eee756cfa9
pgebise <gebise.aiml@gmail.com>
2023-02-16 13:26:28 +0530

    second changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'jfeivr6wgxuw__9bxx^jxsv@u&mxdss+j$y-9ityd8&a#q6hrx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web-production-d3a9b.up.railway.app','localhost','127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
